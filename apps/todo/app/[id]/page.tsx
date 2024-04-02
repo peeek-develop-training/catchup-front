8e89fdaa06ee571bd86f67c15b3be8464774d8af
fujimura1013 <n.fujimura@ei-shin.com>
2024-04-02 19:11:27 +0900

    重複レビュー修正

--- apps/todo/app/[id]/page.tsx
+++ apps/todo/app/[id]/page.tsx
@@ -1,30 +1,51 @@
 "use client";
 
-import { FC } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { FC, useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  item: yup.string().required("タスクを入力してください"),
+});
 
 type Props = {
   params: {
-    id: string;
+    id: number;
   };
 };
 
-type Input = {
-  item: string;
-};
+type Input = yup.InferType<typeof schema>;
+
+type SubmitData = {
+  id: number;
+} & Input;
 
 const ItemPage: FC<Props> = (props) => {
-  const { register, handleSubmit } = useForm<Input>();
+  const [submitData, setSubmitData] = useState<SubmitData>({
+    id: props.params.id,
+    item: "",
+  });
 
-  const onsubmit: SubmitHandler<Input> = (data) => {
-    console.log(data);
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Input>({
+    resolver: yupResolver(schema),
+  });
+
+  const onsubmit = handleSubmit((data) => {
+    setSubmitData((prev) => ({ ...prev, item: data.item }));
+    console.log(submitData);
+  });
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onsubmit)}>
+      <form onSubmit={onsubmit}>
         <input type="text" {...register("item")} />
         <button type="submit">送信</button>
+        {errors.item && <p>{errors.item.message}</p>}
       </form>
     </div>
   );
