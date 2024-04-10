ab0b242be686855d9b8ca1d3707cad86492e59e3
fujimura1013 <n.fujimura@ei-shin.com>
2024-04-10 14:35:19 +0900

    fix storybook/main.ts

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -11,6 +11,12 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-a11y"),
+  ],
   framework: {
     name: getAbsolutePath("@storybook/nextjs"),
     options: {},
